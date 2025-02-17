5261a89d8c63138f0fd9e4f2c6289471f8162bc3
Victor Christensen <microsoft@victorchristensen.dk>
2025-02-17 10:35:18 +0100

    NumPy stuff filled out

---
+++ 0.Dataanalyse/Stocks.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+import os 
+import pandas as pd 
+
+# Load only the 'indexProcessed' data as per the latest analysis requirements
+#stocks = pd.read_csv('G:\\git\\Skole\\Machine Learning\\ml-opgaver-flonk80\\0.Dataanalyse\\Data\\Stocks\Stocks.csv', index_col=0, parse_dates=True) # Dato fra 1980-10-28 til 2021-10-27
+#stocks.head()
+#stocksnew = pd.read_csv('G:\\git\\Skole\\Machine Learning\\ml-opgaver-flonk80\\0.Dataanalyse\\Data\\Stocks\\StocksNew.csv', index_col=0, parse_dates=True) # Dato fra 2022-10-28 til 2024-10-27
+#stocksnew.head()
+
+
+
+
+
+
