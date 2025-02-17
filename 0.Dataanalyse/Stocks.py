8c30ea2757654570bb412792c612e6f30d6312c0
Victor Christensen <microsoft@victorchristensen.dk>
2025-02-17 14:19:18 +0100

    Stocks sorted

--- 0.Dataanalyse/Stocks.py
+++ 0.Dataanalyse/Stocks.py
@@ -1,16 +1,15 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
-import os 
 import pandas as pd 
 
-# Load only the 'indexProcessed' data as per the latest analysis requirements
-#stocks = pd.read_csv('G:\\git\\Skole\\Machine Learning\\ml-opgaver-flonk80\\0.Dataanalyse\\Data\\Stocks\Stocks.csv', index_col=0, parse_dates=True) # Dato fra 1980-10-28 til 2021-10-27
-#stocks.head()
-#stocksnew = pd.read_csv('G:\\git\\Skole\\Machine Learning\\ml-opgaver-flonk80\\0.Dataanalyse\\Data\\Stocks\\StocksNew.csv', index_col=0, parse_dates=True) # Dato fra 2022-10-28 til 2024-10-27
-#stocksnew.head()
+stocks = pd.read_csv('G:\\git\\Skole\\Machine Learning\\ml-opgaver-flonk80\\0.Dataanalyse\\Data\\Stocks\\Stocks.csv', index_col=0, parse_dates=True) # Dato fra 2022-10-28 til 2024-10-27
+stocks = stocks[stocks.index > '2020-01-01']
+stocks = 100 * (stocks / stocks.iloc[0, :])
 
+stocksOrdered = (stocks.iloc[-1] - stocks.iloc[0]).sort_values(ascending=False)
 
+#stocks.plot(loglog=True)
+#plt.show()
 
-
-
-
+print(stocks)
+print(stocksOrdered)
